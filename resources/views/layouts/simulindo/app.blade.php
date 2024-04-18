045d7814877049af513d7203ef0ee35e0cdb0e36
Naufal Faqiih <ardmisters@gmail.com>
2024-04-18 20:29:33 +0700

    Commit pertama SIMULINDO

---
+++ resources/views/layouts/simulindo/app.blade.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+    <head>
+
+        @include('layouts.simulindo.meta')
+
+        @include('layouts.simulindo.style')
+
+    </head>
+<body>
+
+    @yield('modal')
+
+    <main class="main-wrapper">
+        @include('layouts.simulindo.sidebar')
+
+        <div class="page-wrapper">
+            @include('layouts.simulindo.navbar')
+
+            <div class="page-content">
+                @yield('content')
+            </div>
+
+            @include('layouts.simulindo.footer')
+        </div>
+    </main>
+    
+    @include('layouts.simulindo.script')
+</body>
+</html>
