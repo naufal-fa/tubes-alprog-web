db1c46a30f60e27fb81f30bbe1140b93774dbbf1
Rafli Dani <raflidani9@gmail.com>
2024-06-12 23:36:41 +0700

    fix height article card image

--- resources/views/livewire/main/article/article-index.blade.php
+++ resources/views/livewire/main/article/article-index.blade.php
@@ -24,68 +24,76 @@
     <div class="container">
       <div class="row">
 
-        <div class="col-lg-6">
-          <div class="card card-article mb-3">
-            <div class="row g-0">
+        <div class="col-lg-6 mb-3">
+          <div class="card card-article h-100">
+            <div class="d-flex flex-row h-100">
               <div class="col-4 article-cover-img">
                 <img src="{{ asset('images/article-img/article-img-1.png') }}" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-8">
-                <div class="card-body">
-                  <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
-                  <h5 class="card-title fw-bold">Penyebab Lumpur Lapindo di Sidoarjo, Sejarah dan Dampaknya</h5>
-                  <a href="https://www.liputan6.com/hot/read/4892987/penyebab-lumpur-lapindo-di-sidoarjo-sejarah-dan-dampaknya" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                <div class="card-body h-100 d-flex flex-column">
+                  <div class="mb-1">
+                    <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
+                    <h5 class="card-title fw-bold">Penyebab Lumpur Lapindo di Sidoarjo, Sejarah dan Dampaknya</h5>
+                    <a href="https://www.liputan6.com/hot/read/4892987/penyebab-lumpur-lapindo-di-sidoarjo-sejarah-dan-dampaknya" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-6">
-          <div class="card card-article mb-3">
-            <div class="row g-0">
+        <div class="col-lg-6 mb-3">
+          <div class="card card-article h-100">
+            <div class="d-flex flex-row h-100">
               <div class="col-4 article-cover-img">
                 <img src="{{ asset('images/article-img/article-img-2.png') }}" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-8">
-                <div class="card-body">
-                  <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
-                  <h5 class="card-title fw-bold">Ketahui Sejarah Lumpur Lapindo Sidoarjo Beserta Penyebab & Dampaknya bagi Sekitar</h5>
-                  <a href="https://www.merdeka.com/trending/ketahui-sejarah-lumpur-lapindo-sidoarjo-beserta-penyebab-dampaknya-bagi-sekitar-23578-mvk.html" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                <div class="card-body h-100 d-flex flex-column">
+                  <div class="mb-1">
+                    <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
+                    <h5 class="card-title fw-bold">Ketahui Sejarah Lumpur Lapindo Sidoarjo Beserta Penyebab & Dampaknya bagi Sekitar</h5>
+                    <a href="https://www.merdeka.com/trending/ketahui-sejarah-lumpur-lapindo-sidoarjo-beserta-penyebab-dampaknya-bagi-sekitar-23578-mvk.html" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-6">
-          <div class="card card-article mb-3">
-            <div class="row g-0">
+        <div class="col-lg-6 mb-3">
+          <div class="card card-article h-100">
+            <div class="d-flex flex-row h-100">
               <div class="col-4 article-cover-img">
                 <img src="{{ asset('images/article-img/article-img-3.png') }}" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-8">
-                <div class="card-body">
-                  <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
-                  <h5 class="card-title fw-bold">Sudah 16 Tahun Menyembur, Ternyata Ini Awal Lumpur Lapindo!</h5>
-                  <a href="https://www.cnbcindonesia.com/news/20220204112842-4-312865/sudah-16-tahun-menyembur-ternyata-ini-awal-lumpur-lapindo" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                <div class="card-body h-100 d-flex flex-column">
+                  <div class="mb-1">
+                    <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
+                    <h5 class="card-title fw-bold">Sudah 16 Tahun Menyembur, Ternyata Ini Awal Lumpur Lapindo!</h5>
+                    <a href="https://www.cnbcindonesia.com/news/20220204112842-4-312865/sudah-16-tahun-menyembur-ternyata-ini-awal-lumpur-lapindo" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
 
-        <div class="col-lg-6">
-          <div class="card card-article mb-3">
-            <div class="row g-0">
+        <div class="col-lg-6 mb-3">
+          <div class="card card-article h-100">
+            <div class="d-flex flex-row h-100">
               <div class="col-4 article-cover-img">
                 <img src="{{ asset('images/article-img/article-img-4.png') }}" class="img-fluid rounded-start" alt="...">
               </div>
               <div class="col-8">
-                <div class="card-body">
-                  <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
-                  <h5 class="card-title fw-bold">18 Tahun Semburan Lumpur Lapindo, Dosen ITB Pertanyakan Reaktivasi Sesar Watukosek</h5>
-                  <a href="https://tekno.tempo.co/read/1874228/18-tahun-semburan-lumpur-lapindo-dosen-itb-pertanyakan-reaktivasi-sesar-watukosek" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                <div class="card-body h-100 d-flex flex-column">
+                  <div class="mb-1">
+                    <p class="badge bg-success text-white mb-2" style="font-size: 12px;">Fenomena Alam</p>
+                    <h5 class="card-title fw-bold">18 Tahun Semburan Lumpur Lapindo, Dosen ITB Pertanyakan Reaktivasi Sesar Watukosek</h5>
+                    <a href="https://tekno.tempo.co/read/1874228/18-tahun-semburan-lumpur-lapindo-dosen-itb-pertanyakan-reaktivasi-sesar-watukosek" class="btn btn-primary col-12 col-md-6 col-lg-5 mt-3"><p class="d-inline">Baca Artikel</p></a>
+                  </div>
                 </div>
               </div>
             </div>
